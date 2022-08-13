7ed871812f45c745b7a6fc12f7787fd07104fe2f
gitallstara <avr2820@gmail.com>
2022-08-13 17:25:47 -0500

    servlets

--- src/main/java/com/reddy/login_logout/Welcome.java
+++ src/main/java/com/reddy/login_logout/Welcome.java
@@ -3,6 +3,8 @@ package com.reddy.login_logout;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -14,34 +16,32 @@ import javax.servlet.http.HttpSession;
 public class Welcome extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-			
+
 		String username;
 
-
 		String cookieUsername = null, cookiePassword = null;
-		
+
 		HttpSession session = request.getSession();
 		username = (String) session.getAttribute("username");
-		
-		
-		
+
 		Cookie cookiez[] = request.getCookies();
-		for(Cookie cookie : cookiez) {
-			if (cookie.getName().equals("username") ) {
+		for (Cookie cookie : cookiez) {
+			if (cookie.getName().equals("username")) {
 				cookieUsername = cookie.getValue();
 			}
-			if (cookie.getName().equals("password") ) {
+			if (cookie.getName().equals("password")) {
 				cookiePassword = cookie.getValue();
 			}
 		}
-		
-		
-		
+
 		PrintWriter outPrintWriter = response.getWriter();
-		outPrintWriter.println("hey "+username);
-		outPrintWriter.println("hey cookie username : "+cookieUsername);
-		outPrintWriter.println("hey cookie password : "+cookiePassword);
-	
+		outPrintWriter.println("hey " + username);
+		outPrintWriter.println("hey cookie username : " + cookieUsername);
+		outPrintWriter.println("hey cookie password : " + cookiePassword);
 		
-	}
+		
+		session.setAttribute("username", cookieUsername);
+
+			response.sendRedirect("logout");
+	}		
 }
